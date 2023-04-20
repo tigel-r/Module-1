bc5757a5d13b922bfe627a55b3ec364b2e12bc54
tigel-r <tigel-r@mail.ru>
2023-04-20 19:10:51 +0300

    First extension prg.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string MyName = "Андрей";
-Console.WriteLine(MyName);
+byte MyAge = 21;
+bool MyPet = false;
+float ShSize = 41.5;
+
+Console.WriteLine("Мое имя "+MyName);
+Console.WriteLine($"Мне {MyAge} год");
+Console.WriteLine("У тебя есть питомец?"+MyPet);
+Console.WriteLine($"Мой размер ноги {ShSize}");
 
