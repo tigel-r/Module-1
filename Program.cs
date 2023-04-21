0716a12d58fedef95a2682c72963d82ffcbfd7cb
tigel-r <tigel-r@mail.ru>
2023-04-21 17:04:14 +0300

    use "Parse"

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string MyName = "Андрей";
-Console.WriteLine(MyName);
+Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+Console.Write("Введите возраст: ");
+byte age = byte.Parse(Console.ReadLine());
+Console.WriteLine($"Пользователь: имя-{name},\tвозраст-{age}");
+Console.Write("Какой ваш любимый день недели? ");
+
+DayOfWeek BestDay = (DayOfWeek)byte.Parse(Console.ReadLine());
+
+Console.WriteLine("Ваш любимый день недели " + BestDay);
+Console.ReadKey();
 
