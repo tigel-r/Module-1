47679a0caf78d2e62fea25a6eda6892bd1eaaa44
tigel-r <tigel-r@mail.ru>
2023-04-21 17:13:27 +0300

    Task completed

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.Write("Введите полную дату рождения: ");
+string DateBrd = Console.ReadLine();
+
 Console.Write("Введите имя: ");
 string name = Console.ReadLine();
+
 Console.Write("Введите возраст: ");
 byte age = byte.Parse(Console.ReadLine());
+
 Console.WriteLine($"Пользователь: имя-{name},\tвозраст-{age}");
-Console.Write("Какой ваш любимый день недели? ");
-
-DayOfWeek BestDay = (DayOfWeek)byte.Parse(Console.ReadLine());
-
-Console.WriteLine("Ваш любимый день недели " + BestDay);
-Console.ReadKey();
-
+Console.WriteLine("Дата рождения: " + DateBrd);
