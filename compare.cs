10d4f9245834120e0c32328a92af2246d4317a87
tigel-r <tigel-r@mail.ru>
2023-04-24 17:48:51 +0300

    use logic

---
+++ compare.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+
+bool c = a == b;
+
+Console.WriteLine(c);
+
+Console.ReadKey();
