a909d38605d561a2ae93fe1241172c2d38311cd1
tigel-r <tigel-r@mail.ru>
2023-04-26 22:53:47 +0300

    use array

---
+++ mussiv_tipo.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+
+
+int Val;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = i + 1; j < arr.Length; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            Val = arr[i];
+            arr[i] = arr[j];
+            arr[j] = Val;
+        }
+    }
+    Console.WriteLine(arr[i]);
+}
+
+
+
+
+
+
+
+
