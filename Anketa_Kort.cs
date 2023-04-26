9f276e7b12ff006a98f8862cda8f45adc401f8df
tigel-r <tigel-r@mail.ru>
2023-04-27 00:08:47 +0300

    new "Korteg"

--- Anketa_Kort.cs
+++ Anketa_Kort.cs
@@ -1,17 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-(string name, int age, string DateBrd) Anketa;
-Console.Write("Введите полную дату рождения: ");
-Anketa.DateBrd = Console.ReadLine();
+(string Name, string Type, double Age, int NameCount) Pet;
+Console.Write("Ввведите имя питомца: ");
+Pet.Name = Console.ReadLine();
 
-Console.Write("Введите имя: ");
-Anketa.name = Console.ReadLine();
+Console.Write("Введите вид питомца: ");
+Pet.Type = Console.ReadLine();
 
-Console.Write("Введите возраст: ");
-Anketa.age = byte.Parse(Console.ReadLine());
+Console.Write("Введите возраст питомца: ");
+Pet.Age = double.Parse(Console.ReadLine());
 
-Console.WriteLine($"Пользователь: имя-{Anketa.name},\tвозраст-{Anketa.age}");
-Console.WriteLine("Дата рождения: " + Anketa.DateBrd);
+Pet.NameCount = Pet.Name.Length;
+
+Console.WriteLine();
+Console.WriteLine("Ваш питомец {0}, Его имя {1}", Pet.Type, Pet.Name);
+Console.WriteLine("Возраст вашего питомца: " + Pet.Age);
+Console.WriteLine($"Его имя состоит из {Pet.NameCount} символов");
 
 Console.Read();
