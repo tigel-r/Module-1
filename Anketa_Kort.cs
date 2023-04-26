68624d6a8b19800c33c26edf5b62945dd7d5c7af
tigel-r <tigel-r@mail.ru>
2023-04-26 23:19:32 +0300

    use "Korteg??"

---
+++ Anketa_Kort.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+(string name, int age, string DateBrd) Anketa;
+Console.Write("Введите полную дату рождения: ");
+Anketa.DateBrd = Console.ReadLine();
+
+Console.Write("Введите имя: ");
+Anketa.name = Console.ReadLine();
+
+Console.Write("Введите возраст: ");
+Anketa.age = byte.Parse(Console.ReadLine());
+
+Console.WriteLine($"Пользователь: имя-{Anketa.name},\tвозраст-{Anketa.age}");
+Console.WriteLine("Дата рождения: " + Anketa.DateBrd);
+
+Console.Read();
