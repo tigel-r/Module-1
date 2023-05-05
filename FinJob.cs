c8cabf8c6f23dc09549565be476aca32789f975a
tigel-r <tigel-r@mail.ru>
2023-05-06 00:29:01 +0300

    The second working part of the assignment

--- FinJob.cs
+++ FinJob.cs
@@ -1,61 +1,61 @@
-﻿namespace ItogRab
+﻿using System.Reflection.Metadata;
+
+namespace ItogRab
 {
     internal class FinJob
     {
-        static void AnketInfo()
+        private static void AnketInfo()
         {
-            while (true)
+
+            (string Name, string Surname, double Age, bool HasPet, string[] PetName, string[] Colors) User;
+
+
+            Console.Write("Введите имя: ");
+            User.Name = Console.ReadLine();
+
+            Console.Write("Введите фамилию: ");
+            User.Surname = Console.ReadLine();
+
+            Console.Write("Введите свой возраст: ");
+            User.Age = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.Write("Есть ли у вас питомец? (Да/Нет) : ");
+            string PetInfo = Console.ReadLine();
+
+            switch (PetInfo)
             {
-                Console.Clear();
+                case "Да":
+                    User.HasPet = true;
+                    Console.Write("Сколько у вас питомцев: ");
+                    int PetQuan = int.Parse(Console.ReadLine());
+                    PetQuan = Examination(PetQuan);
+                    User.PetName = NameOfPet(PetQuan);
+                    break;
 
-                (string Name, string Surname, double Age, bool HasPet, string[] PetName, string[] Colors) User;
+                case "Нет":
+                    User.HasPet = false;
+                    User.PetName = new string[0];
+                    break;
 
-
-                Console.Write("Введите имя: ");
-                User.Name = Console.ReadLine();
-
-                Console.Write("Введите фамилию: ");
-                User.Surname = Console.ReadLine();
-
-                Console.Write("Введите свой возраст: ");
-                User.Age = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("");
-
-                Console.Write("Есть ли у вас питомец? (Да/Нет) : ");
-                string PetInfo = Console.ReadLine();
-
-                switch (PetInfo)
-                {
-                    case "Да":
-                        User.HasPet = true;
-                        Console.Write("Сколько у вас питомцев: ");
-                        int PetQuan = int.Parse(Console.ReadLine());
-                        User.PetName = NameOfPet(PetQuan);
-                        break;
-
-                    case "Нет":
-                        User.HasPet = false;
-                        Console.WriteLine("У вас нет питомцев");
-                        break;
-
-                    default:
-                        Console.WriteLine("Я вас не понял((( Повторите попытку");
-                        Console.ReadKey();
-                        continue;
-                }
-
-                Console.WriteLine("");
-
-                Console.WriteLine("Сколько у вас любимых цветов?");
-                int ColorQuan = int.Parse(Console.ReadLine());
-
-                User.Colors = NameOfColor(ColorQuan);
-
-
-                Console.ReadKey();
-
+                default:
+                    Console.WriteLine("Я вас не понял(((");
+                    User.HasPet = false;
+                    User.PetName = new string[0];
+                    Console.ReadKey();
+                    break;
             }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Сколько у вас любимых цветов?");
+            int ColorQuan = int.Parse(Console.ReadLine());
+            ColorQuan = Examination(ColorQuan);
+
+            User.Colors = NameOfColor(ColorQuan);
+
+            Console.ReadKey();
 
         }
 
@@ -86,14 +86,55 @@
             return Colors;
         }
 
-        static void Examination()
+        static int Examination(int CorrNum)
         {
-        
+
+            if (CorrNum > 0)
+            { return CorrNum; }
+            else 
+            {
+                Console.WriteLine("Данные не коректны... Попробуйте снова");
+                CorrNum = int.Parse(Console.ReadLine());
+                CorrNum = Examination(CorrNum);
+                return CorrNum;
+            
+            }
+
+        }
+
+        static void AnketСonclusion((string Name, string Surname, double Age, bool HasPet, string[] PetName, string[] Colors) UserInfo) 
+        {
+            Console.WriteLine($"Имя пользователя: {UserInfo.Name} {UserInfo.Surname}");
+            Console.WriteLine($"Возраст пользователя: {UserInfo.Age}");
+            Console.WriteLine("");
+
+            if (UserInfo.HasPet)
+            {
+                Console.WriteLine("Ваши питомцы: ");
+                for (int i = 0; i < UserInfo.PetName.Length; i++)
+                {
+                    Console.WriteLine(UserInfo.PetName[i]);
+                }
+            }
+            else
+            { Console.WriteLine("Пожоже у вас питомцев нет"); }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Ваши любимые цвета: ");
+            for (int i = 0; i < UserInfo.Colors.Length; i++)
+            {
+                Console.WriteLine(UserInfo.Colors[i]);
+            }
+
+            Console.ReadLine();
         }
 
         static void Main(string[] args)
         {
             AnketInfo();
+            Console.WriteLine("");
+            
 
         }
 
